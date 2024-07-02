cc3fbc3eb1969e0674c1aad499e0c09dcf48d2ce
cyburns <72774499+cyburns@users.noreply.github.com>
2024-07-02 13:33:57 -0400

    fa

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="  example">
-      <Mouse />
+      {/* <Mouse /> */}
       <body className={`${raleway.className} `}>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
