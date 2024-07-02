68be00f0c0122e88f6051d1e09bd3e2a3b28f840
cyburns <72774499+cyburns@users.noreply.github.com>
2024-07-02 14:52:05 -0400

    Fa

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,12 +1,47 @@
 import React from "react";
 import Image from "next/image";
-import CB from "@/public/images/cb-blur-2.png";
+import CB from "@/public/cb-motion-blur.png";
+import { motion } from "framer-motion";
+
+const name = "CYRUS";
+
+const textVariants = {
+  hidden: {
+    y: 400,
+    rotateX: -50,
+    transformOrigin: "bottom",
+  },
+  visible: (i: number) => ({
+    y: 0,
+    rotateX: 0,
+    transition: {
+      delay: i * 0.15,
+      duration: 0.5,
+      type: "spring",
+      stiffness: 70,
+      damping: 30,
+    },
+  }),
+};
 
 const Hero = () => {
   return (
     <div className="h-screen w-screen bg-gradient-to-r from-[#38464a] sm:from-[#0c110f] via-[#38464a] sm:via-[#38464a] to-[#39494e] sm:to-[#39494e] overflow-hidden">
-      <div className="text-white flex justify-center items-center w-full h-full flex-col uppercase relative">
-        <h1 className="text-[23vw] font-black z-50 sm:z-0">CYRUS</h1>
+      <div className=" text-white flex justify-center items-center w-full h-full flex-col uppercase relative">
+        <div className="z-50  container-1 overflow-hidden w-screen flex justify-center">
+          {name.split("").map((letter, index) => (
+            <motion.h1
+              key={index}
+              className="text-[23vw] font-black !leading-[0.75]"
+              initial="hidden"
+              animate="visible"
+              custom={index}
+              variants={textVariants}
+            >
+              {letter}
+            </motion.h1>
+          ))}
+        </div>
         <div className="flex justify-between absolute z-50 bottom-[40%] sm:bottom-[28%] w-[76.5%] text-xs sm:text-base">
           <p>
             Developer / <br /> Designer
@@ -16,11 +51,11 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <div className="absolute bottom-0 ">
+      <div className="absolute bottom-0">
         <Image
           src={CB}
           alt="CB"
-          className="z-10 h-[100vh] object-cover w-screen"
+          className="z-10 h-[100vh] object-contain w-screen scale-125"
         />
       </div>
     </div>
